2d6f3cc4374a45f9ab5d3cc198d8b92004382e23
zohaib.ijaz <zohaib.ijaz@northbaysolutions.net>
2018-04-20 15:47:43 +0500

    Fixed a bug

--- index.js
+++ index.js
@@ -1,54 +1,56 @@
-const path = require('path');
-const theme = require('./antd-theme');
+const path = require("path");
+const theme = require("./antd-theme");
 
 class AntDesignThemePlugin {
   constructor(options) {
     const defaulOptions = {
-      varFile: path.join(__dirname, '../../src/styles/variables.less'),
-      mainLessFile: path.join(__dirname, '../../src/styles/index.less'),
-      antDir: path.join(__dirname, '../../node_modules/antd'),
-      stylesDir: path.join(__dirname, '../../src/styles/antd'),
-      themeVariables: ['@primary-color'],
-      indexFileName: 'index.html'
-    }
-    this.options = { ...defaulOptions, ...(options || {}) };
+      varFile: path.join(__dirname, "../../src/styles/variables.less"),
+      mainLessFile: path.join(__dirname, "../../src/styles/index.less"),
+      antDir: path.join(__dirname, "../../node_modules/antd"),
+      stylesDir: path.join(__dirname, "../../src/styles/antd"),
+      themeVariables: ["@primary-color"],
+      indexFileName: "index.html"
+    };
+    this.options = Object.assign(defaulOptions, options);
   }
 
   apply(compiler) {
     const options = this.options;
-    compiler.plugin('emit', function (compilation, callback) {
-      theme.generateColorLess(options).then(css => {
-        const less = `
+    compiler.plugin("emit", function(compilation, callback) {
+      theme
+        .generateColorLess(options)
+        .then(css => {
+          const less = `
         <link rel="stylesheet/less" type="text/css" href="/color.less" />
         <script>
           window.less = {
-            async: true,
+            async: false,
             env: 'production'
           };
         </script>
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/less.js/2.7.2/less.min.js"></script>
         `;
-        if (options.indexFileName in compilation.assets) {
-          const index = compilation.assets[options.indexFileName];
-          let content = index.source();
+          if (options.indexFileName in compilation.assets) {
+            const index = compilation.assets[options.indexFileName];
+            let content = index.source();
 
-          if (!content.match(/\/color\.less/g)) {
-            index.source = () => content.replace(less, '').replace(/<\/body>/ig, less + '</body>');
-            content = index.source();
-            index.size = () => content.length;
+            if (!content.match(/\/color\.less/g)) {
+              index.source = () =>
+                content.replace(less, "").replace(/<body>/gi, "<body>" + less);
+              content = index.source();
+              index.size = () => content.length;
+            }
           }
-        }
 
-        compilation.assets['color.less'] = {
-          source: () => css,
-          size: () => css.length
-        }
-        callback();
-      })
-        .catch(err => {
-          callback(err)
+          compilation.assets["color.less"] = {
+            source: () => css,
+            size: () => css.length
+          };
+          callback();
         })
-
+        .catch(err => {
+          callback(err);
+        });
     });
   }
 }
