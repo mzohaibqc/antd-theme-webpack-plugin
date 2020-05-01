9fbca143e2fb132e4e7f0b3a18b1aeaa51eb40aa
Zohaib Ijaz <mzohaib.qc@gmail.com>
2020-05-01 07:22:28 +0500

    Added build content in root

---
+++ precache-manifest.5c5bef1c526587dc8a922952798a7898.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d8f96637794664facf4d6aedb7b814b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6b0f66bc35be68de40f",
+    "url": "/static/css/2.52a14649.chunk.css"
+  },
+  {
+    "revision": "7ef443a001af03020067",
+    "url": "/static/css/main.43067c35.chunk.css"
+  },
+  {
+    "revision": "a6b0f66bc35be68de40f",
+    "url": "/static/js/2.f9a414df.chunk.js"
+  },
+  {
+    "revision": "5ab1baa52e351a54fb487f180027ee45",
+    "url": "/static/js/2.f9a414df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ef443a001af03020067",
+    "url": "/static/js/main.8d42e969.chunk.js"
+  },
+  {
+    "revision": "aaf411ce36fe10516ed0",
+    "url": "/static/js/runtime-main.b2946e44.js"
+  }
+]);
